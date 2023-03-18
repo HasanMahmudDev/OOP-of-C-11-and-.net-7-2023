4b719e47cfcb31e4cbfbba10572969980eb1e965
HasanMahmudDev <hasanmahmuddev@gmail.com>
2023-03-18 21:14:32 +0600

    Add New Command

--- OOP/Person.cs
+++ OOP/Person.cs
@@ -1,18 +1,25 @@
-﻿
-class Person
+﻿public class Person // Class & Interface By Default Internal Modifire
 {
          //এখানে দুটি বিষয়: ১. ফিল্ড ২. মেথড
-         string name;// Field king of variable 
-         string age; // অন্য নাম ডাটা
+       public  string name;// Field king of variable 
+            // Field & Method By Default Private Modifire
+            // 1. Public -- যে কেহ Access করতে পারবে।
+            //2. Private --  শুধু বাউন্ডারির ভিতরে ব্যবহার করা যাবে।
+            //3. Protected -- 
+            //4. Internal -- Same Project a thakle paya jabe.
+            /// <summary>
+            /// 
+            /// </summary>
+       public  string age; // অন্য নাম ডাটা
         // Funtion allways Returnable
         // vb.net subrootine Return করলে একটা আর ‍না করলে 
         // মেথড এবং ফাংশন এর মধে তেমন কোন পার্থক্য নেই।
 
-    void Walk() // Void Method Non Returnable // Method Other Name Like Oparation 
+   public  void Walk() // Void Method Non Returnable // Method Other Name Like Oparation 
     {
         Console.WriteLine();
     }
-    void Talk() // 
+    public  void Talk() // 
     {
 
     }
@@ -23,7 +30,7 @@ class Person
 
     //কোন ইম্পিমেনটেশন কে হাইড করা মানে Abstruction
 
-
+    
 
      
            
--- OOP/Program.cs
+++ OOP/Program.cs
@@ -5,5 +5,7 @@ public class Program
     public static void main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        Person p = new Person();
+        p.age = 45;
     }
 }
